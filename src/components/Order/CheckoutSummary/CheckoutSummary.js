a6231acaab444e5332c30f55bf35e63f223f7a30
Saif <saifsadiq.ss@gmail.com>
2021-03-12 23:15:03 +0500

    order

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>EETHAY NAAME DALU GA MAE</h1>
+            <div></div>
+        </div>
+    )
+}
