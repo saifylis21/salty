44e531fb930432c5a0df662a88fa1f82eb48e2d3
Saif <saifsadiq.ss@gmail.com>
2021-03-24 23:18:54 +0500

    doing routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
 import Home from './containers/Home/Home';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
-        <Home />
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={Home} />
+        </Switch>
       </Layout>
     </div>
   );
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 
 class Checkout extends Component {
-    
+
+    // state = the order i have placed and then might pass on to other components
+
+
+    render() {
+        return (
+            <div>
+                <h1>this is checkout shit</h1>
+                <h1>this is checkout shit</h1>
+                <h1>this is checkout shit</h1>
+                <h1>this is checkout shit</h1>
+                <h1>this is checkout shit</h1>
+            </div>
+        )
+    }
 }
 
 export default Checkout;
