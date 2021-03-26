456bc18386a3574dc42651b66832666d7ea12f77
Saif <saifsadiq.ss@gmail.com>
2021-03-26 22:49:10 +0500

    link placed in a better place

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import CategoryCard from './CategoryCard/CategoryCard';
 
@@ -9,12 +8,9 @@ const Categories = (props) => {
     categories = Object.keys(props.categories).map((categoryKey) => {
         let category = props.categories[categoryKey];
         return (
-            <Link key={categoryKey} to={`/browse/${category.name}`}>
-                <CategoryCard name={category.name} img={category.imageURL} />
-            </Link>
+            <CategoryCard key={categoryKey} name={category.name} img={category.imageURL} />
         )
     });
-
 
     return (
         <div>
--- src/components/Categories/CategoryCard/CategoryCard.js
+++ src/components/Categories/CategoryCard/CategoryCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import classes from './CategoryCard.module.css'
 
 const CategoryCard = (props) => (
-    <div className={classes.CategoryCard}>
-        <img src={props.img} alt="" />
-        <h3>{props.name}</h3>
-    </div>
+    <Link to={`/browse/${props.name}`}>
+        <div className={classes.CategoryCard}>
+            <img src={props.img} alt="" />
+            <h3>{props.name}</h3>
+        </div>
+    </Link>
 )
 
 export default CategoryCard;
