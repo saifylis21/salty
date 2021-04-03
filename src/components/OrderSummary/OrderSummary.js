3acd35955e6b424d8de3be8c5b333a0b967a5167
Saif <saifsadiq.ss@gmail.com>
2021-04-03 14:26:17 +0500

    order summary showing

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const OrderSummary = (props) => {
-
-
     return (
-        <h3>Your order</h3>
+        <div>
+            <h3>{props.name}</h3>
+            <h4>Quantity: {props.order.quantity}</h4>
+            <h4>Total Price: {props.order.totalPrice}</h4>
+        </div>
     );
 };
 
--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../axios-main';
 
+import OrderSummary from '../../components/OrderSummary/OrderSummary';
 import ProductCard from './ProductCard/ProductCard';
 import Button from '../../components/UI/Button/Button';
+import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../../hoc/Aux/Aux';
 
 
 const Product = (props) => {
     const productId = props.match.params.id;
     const [product, setProduct] = useState({});
-    const [showModal, setShowModal] = useState(false);
-    console.log(showModal)
+    const [order, setOrder] = useState({
+        totalPrice: 0,
+        quantity: 0
+    });
+    const [purchasing, setPurchasing] = useState(false);
 
     useEffect(() => {
         axios.get('/cardInfo.json')
@@ -23,6 +28,10 @@ const Product = (props) => {
                         name: item.name,                        
                         price: item.price             
                     });
+                    setOrder({
+                        totalPrice: item.price,
+                        quantity: 1
+                    });
                 }
             });
         })
@@ -31,18 +40,33 @@ const Product = (props) => {
         });
     }, [productId]);
 
+    const purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
+
+    const purchaseCancelHandler = () => {
+        setPurchasing(false)
+    }
+
     let card = null;
     if(product) {
         card = <Aux>
             <ProductCard imgURL={product.imgURL} name={product.name} price={product.price} />
-            <Button clicked={() => {setShowModal(true)}}>Order now</Button>
+            <Button clicked={() => {setPurchasing(true)}}>Order now</Button>
         </Aux>
     }
 
     return (
-        <div>
+        <Aux>
+            <Modal show={purchasing} modalClosed={purchaseCancelHandler}>
+                <OrderSummary
+                    name={product.name}
+                    order={order}
+                    continue={purchaseContinueHandler}
+                />
+            </Modal>
             {card}
-        </div>
+        </Aux>
     );
 
 };
