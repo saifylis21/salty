61afe20b0beacd40a529877f75bd4e7597d3c2fb
Saif <saifsadiq.ss@gmail.com>
2021-04-17 22:49:18 +0500

    generic form made

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,18 +1,31 @@
-import classes from '*.module.css';
 import React from 'react';
+
+import classes from './Input.module.css';
 
 const Input = (props) => {
     let inputElement = null;
 
     switch(props.elementType) {
         case('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input 
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
             break;
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <textarea 
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input 
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
     }
 
     return (
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,80 +1,82 @@
 import React, {useEffect, useState} from 'react';
 
+import Input from '../../components/UI/Input/Input';
+
 const Checkout = (props) => {
 
-    // const [orderForm, setOrderForm] = useState({
-    //     name: {
-    //         elementType: 'input',
-    //         elementConfig: {
-    //             type: 'text',
-    //             placeholder: 'Name'
-    //         },
-    //         value: '',
-    //         validation: {
-    //             required: true,
-    //         },
-    //         valid: false,
-    //         touched: false
-    //     },
+    const [orderForm, setOrderForm] = useState({
+        name: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Name'
+            },
+            value: '',
+            validation: {
+                required: true,
+            },
+            valid: false,
+            touched: false
+        },
 
-    //     phone: {
-    //         elementType: 'input',
-    //         elementConfig: {
-    //             type: 'text',
-    //             placeholder: 'Contact No'
-    //         },
-    //         value: '',
-    //         validation: {
-    //             required: true,
-    //             minLength: 6
-    //         },
-    //         valid: false,
-    //         touched: false
-    //     },
+        phone: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Contact No'
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 6
+            },
+            valid: false,
+            touched: false
+        },
 
-    //     address1: {
-    //         elementType: 'input',
-    //         elementConfig: {
-    //             type: 'text',
-    //             placeholder: 'Address Line 1'
-    //         },
-    //         value: '',
-    //         validation: {
-    //             required: true,
-    //             minLength: 8
-    //         },
-    //         valid: false,
-    //         touched: false
-    //     },
+        address1: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Address Line 1'
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 8
+            },
+            valid: false,
+            touched: false
+        },
 
-    //     address2: {
-    //         elementType: 'input',
-    //         elementConfig: {
-    //             type: 'text',
-    //             placeholder: 'Address Line 2'
-    //         },
-    //         value: '',
-    //         validation: {
-    //             required: false,
-    //         },
-    //         valid: true,
-    //         touched: false
-    //     },
+        address2: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Address Line 2'
+            },
+            value: '',
+            validation: {
+                required: false,
+            },
+            valid: true,
+            touched: false
+        },
 
-    //     city: {
-    //         elementType: 'input',
-    //         elementConfig: {
-    //             type: 'text',
-    //             placeholder: 'City'
-    //         },
-    //         value: '',
-    //         validation: {
-    //             required: false,
-    //         },
-    //         valid: false,
-    //         touched: false
-    //     },
-    // });
+        city: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'City'
+            },
+            value: '',
+            validation: {
+                required: false,
+            },
+            valid: false,
+            touched: false
+        },
+    });
 
     useEffect(() => {
         // const query = new URLSearchParams(props.location.search);
@@ -84,10 +86,47 @@ const Checkout = (props) => {
     },[])
 
 
+
+    const inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...orderForm,
+            [inputIdentifier]: {
+                ...orderForm[inputIdentifier],
+                value: event.target.value
+            }
+        }
+        setOrderForm(updatedOrderForm);
+    }
+
+    const formElementsArray = [];
+
+    for (let key in orderForm) {
+        formElementsArray.push({
+            id: key,
+            config: orderForm[key]
+        });
+    }
+
+    let form = formElementsArray.map(formElement => (
+        <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => inputChangedHandler(event, formElement.id)}
+            // shouldValidate={formElement.config.validation}
+            // invalid={!formElement.config.valid}
+            // touched={formElement.config.touched}
+        />
+    ));
+
+
     return (
         <div>
             <h1>check dis shet mothertrucker</h1>
-            <h1>FORM</h1>
+            <h1>check dis shet mothertrucker</h1>
+            <h1>check dis shet mothertrucker</h1>
+            {form}
             <h1>CHECKOUT SUMMARY</h1>
         </div>
     )
